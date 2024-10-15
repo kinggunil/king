5f60f1ff4493008ed517b116a13ef339acdc3fd4
kinggunil <kinggunil@kinggunilui-MacBookAir.local>
2024-10-15 14:44:38 +0900

    first commit

--- src/lib.rs
+++ src/lib.rs
@@ -114,6 +114,6 @@ fn main() {
     let a=king::k_get_url("https://api.binance.com/api/v3/ticker/price");
     let b=a.unwrap();
     let c:serde_json::Value=serde_json::from_str(&b).unwrap();
-    println!("{}",c[0]);
+    println!("{}",c[0]);//
 }
  */
